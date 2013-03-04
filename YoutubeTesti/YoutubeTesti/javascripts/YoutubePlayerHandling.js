d01c088c6d5a981e3d89b3721311eabcef1899f2
Teemu Sarapisto <tsarpf@gmail.com>
2013-03-04 23:54:29 +0200

    Working build, but javascript is ugly spaghetti.
    Plx rewrite me.

--- YoutubeTesti/YoutubeTesti/javascripts/YoutubePlayerHandling.js
+++ YoutubeTesti/YoutubeTesti/javascripts/YoutubePlayerHandling.js
@@ -5,7 +5,9 @@ var videoTitleDivList = new Array();
 var currentVideoIdx = 0;
 var currentVideoLength = 0;
 var videoStateTimer;
+var backupVideoList = new Array();
 var overHalfway = false;
+var videosPlayed = 0;
 
 var createNewPlayer = function (count)
 {
@@ -36,7 +38,9 @@ function onYouTubePlayerReady() //executed when onYouTubePlayerReady() is, hence
     //ytplayers[ytplayers.length - 1].addEventListener("onStateChange", "onPlayerStateChange"); //onPlayerStateChange should be replaced with our own function's name
     //ytplayers[ytplayers.length - 1].addEventListener("onError", "onPlayerError");             //onPlayerError should be replaced with our own function's name
 
-    ytplayers[ytplayers.length - 1].addEventListener("onStateChange", "onPlayerStateChange");
+    //ytplayers[ytplayers.length - 1].addEventListener("onStateChange", "onPlayerStateChange");
+    console.log("cueing video: " + videoList[0]);
+    backupVideoList.push(videoList[0]);
     ytplayers[ytplayers.length - 1].cueVideoById(videoList.shift(), 0, "highres"); //shift() Removes the first item of the array, and returns that item.
 
 }
@@ -59,23 +63,44 @@ function setVideoLength()
 {
     var length = ytplayers[currentVideoIdx].getDuration();
     if (length != 0) {
+        console.log("video length set to: " + length);
         currentVideoLength = length;
     }
     else {
+        console.log("length was '" + length + "' not set");
+        checkAndFixVideoCue();
         setTimeout("setVideoLength()", 500);
     }
 }
 
+function checkAndFixVideoCue()
+{
+    var videoURL = ytplayers[currentVideoIdx].getVideoUrl();
+    console.log("URL: " + videoURL);
+    if (videoURL.indexOf(backupVideoList[videosPlayed]) < 0) {
+        console.log("player doesn't have a video, setting one");
+        ytplayers[currentVideoIdx].cueVideoById(backupVideoList[videosPlayed], 0, "highres");
+        ytplayers[currentVideoIdx].playVideo();
+    }
+}
+
 function removeLastVideo() {
-    if (ytplayers[0].getPlayerState() == 0) {
+    var playerState = ytplayers[0].getPlayerState();
+    console.log("playerstate: " + playerState);
+    if (playerState == 0) {
+        console.log("playerstate: " + playerState + " currentvideoidx: " + currentVideoIdx + " ytplayers.length: " + ytplayers.length);
         var mainDiv = document.getElementById("mainDiv");
         mainDiv.removeChild(document.getElementById(videoDivList.shift())); //remove the div containing the video
         mainDiv.removeChild(document.getElementById(videoTitleDivList.shift())); //remove the title div of the video
         ytplayers.shift(); //remove the player from the player list
         currentVideoIdx--; //because all indices in the player list are moved one backwards, we have to compensate
     }
+    else if (playerState == -1) {
+        ytplayers[0].playVideo();
+        //setTimeout("removeLastVideo",500);
+    }
     else {
-        setTimeout("removeLastVideo", 500);
+        setTimeout("removeLastVideo()", 500);
     }
 
 }
@@ -88,8 +113,13 @@ function checkVideoState()
             ytplayers[currentVideoIdx + 1].playVideo();
             ytplayers[currentVideoIdx + 1].pauseVideo();
             overHalfway = true;
+            console.log(ytplayers[currentVideoIdx].getCurrentTime() + " / " + currentVideoLength + " through video");
         }
-        else if (currentVideoLength - ytplayers[currentVideoIdx].getCurrentTime() <= 2) {
+        else if (currentVideoLength - ytplayers[currentVideoIdx].getCurrentTime() <= 1) {
+            if (ytplayers[currentVideoIdx].getPlayerState() == -1) {
+                ytplayers[currentVideoIdx].playVideo();
+                return;
+            }
             playNextVideo();
         }
     }
@@ -100,6 +130,7 @@ function checkVideoState()
 
 function playNextVideo() {
     currentVideoIdx++;
+    videosPlayed++;
     ytplayers[currentVideoIdx].playVideo();
     currentVideoLength = 999999;
     setTimeout("setVideoLength()", 500);
