7cdf5d7791ca3fd581daf68ea6e94f3f0d5ff79d
Teemu Sarapisto <tsarpf@gmail.com>
2013-04-15 07:06:05 +0300

    "Logging in" now works at /login/ with testuser:testpassword

--- YouTubeBootstrapped/MvcWebRole1/Controllers/LoginController.cs
+++ YouTubeBootstrapped/MvcWebRole1/Controllers/LoginController.cs
@@ -31,6 +31,8 @@ namespace YOUTUBEiMPROVED.Controllers
 
             SqlConnectionStringBuilder csBuilder = null;
 
+			string serverPassword = @"!!!!!!!!(/)Q&)(/&¤";
+            string givenPassword = "";
 			try
 			{
 				//csBuilder = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
@@ -56,46 +58,40 @@ namespace YOUTUBEiMPROVED.Controllers
 			try
 			{
 				SqlDataReader reader = null;
-				SqlCommand command = new SqlCommand("select * from \"User\"", connection);
+
+				givenPassword = login.password;
+				string tinyBitSaferUsername = login.username.Replace("'", "''"); //Weak protection from SQL injections
+
+				SqlCommand command = new SqlCommand("SELECT Password FROM UserTable WHERE UserName = '"
+					+ tinyBitSaferUsername + "'"
+					, connection);
 				reader = command.ExecuteReader();
 
 				while (reader.Read())
 				{
 					IDataRecord asd = (IDataRecord)reader;
-					for (int i = 0; i < asd.FieldCount; i++)
-					{
-						login.loginMessage += asd[i] + " ";
-					}
-					login.loginMessage += "\n";
+                    if(asd[0].ToString().Length > 0)
+					    serverPassword = asd[0].ToString();
 				}
 			}
 			catch (Exception e)
 			{
-				login.loginMessage = e.Message;
+				login.correct = false;
+				login.loginMessage = "Invalid username or password";
 			}
-
-            /*
-			//Retrieve column schema into a DataTable.
-			DataTable schemaTable = reader.GetSchemaTable();
-
-			//For each field in the table...
-			foreach (DataRow myField in schemaTable.Rows)
-			{
-				//For each property of the field...
-				foreach (DataColumn myProperty in schemaTable.Columns)
-				{
-					//Display the field name and value.
-					login.loginMessage += myProperty.ColumnName + " = " + myField[myProperty].ToString() + "\n";
-				}
-				login.loginMessage += "\n";
-
-			}
-            */
 
 			connection.Close();
 
-
-			login.correct = false;
+			if (serverPassword == givenPassword)
+			{
+				login.correct = true;
+				login.loginMessage = "Login succesful";
+			}
+			else
+			{
+				login.correct = false;
+				login.loginMessage = "Invalid username or password";
+			}
 
 
 			return View("Login", login);
